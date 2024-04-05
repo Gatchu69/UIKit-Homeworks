f136408cd3c165f74963291dcaa6dfa1c467efb5
Nodiko Gatchava <ghachava.nodar@tbcacademy.edu.ge>
2024-04-06 00:11:02 +0400

    50% of stolen design

---
+++ homework2UIKit/homework2UIKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  homework2UIKit
+//
+//  Created by Nodiko Gachava on 05.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+    
+    
+}
+
+
+
+
+
+
