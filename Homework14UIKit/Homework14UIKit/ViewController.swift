562a182b49d74d8eb43923ae4be9c8ebcda12958
Nodiko Gatchava <ghachava.nodar@tbcacademy.edu.ge>
2024-04-08 03:41:44 +0400

    New assignment14

---
+++ Homework14UIKit/Homework14UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework14UIKit
+//
+//  Created by Nodiko Gachava on 07.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
